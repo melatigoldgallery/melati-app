94d55d6e65b3bc263647a9d44f3189ec22e905e4
unknown <fattahula98@gmail.com>
2025-11-02 10:16:21 +0800

    update box print service

--- js/pages/input-servis.js
+++ js/pages/input-servis.js
@@ -725,8 +725,8 @@ function getPrintStyles() {
         gap: 3mm;
       }
       .print-service-box {
-        width: 4.5cm;
-        height: 4.5cm;
+        width: 3.5cm;
+        height: 3.5cm;
         border: 1px solid #000;
         padding: 2mm;
         box-sizing: border-box;
