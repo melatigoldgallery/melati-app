36bd02f3c613a2c5aed8ed5d1df2e2129f22c92c
melatigoldgallery <melatigoldshopid@gmail.com>
2025-12-24 13:35:41 +0800

    update config

--- js/configFirebase.js
+++ js/configFirebase.js
@@ -12,7 +12,17 @@ import {
   signOut,
   onAuthStateChanged,
 } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js'
-import { firebaseConfig } from './firebase-config.private.js'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyB6WS177m4mFIIlDE9sSSW21XHkWHQdwdU',
+  authDomain: 'sistem-antrian-76aa8.firebaseapp.com',
+  databaseURL: 'https://sistem-antrian-76aa8-default-rtdb.asia-southeast1.firebasedatabase.app',
+  projectId: 'sistem-antrian-76aa8',
+  storageBucket: 'sistem-antrian-76aa8.firebasestorage.app',
+  messagingSenderId: '545586001781',
+  appId: '1:545586001781:web:cae2e84ad9c8d905c7053c',
+  measurementId: 'G-2SYW19ZQD8',
+}
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
